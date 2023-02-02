cbd311114f21dde5cad8f44ad402c70cfaa5c305
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-02-02 11:05:17 -0800

    working on marking goal complete

--- Assets/Scripts/DisplayGoal.cs
+++ Assets/Scripts/DisplayGoal.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
-public class DisplayGoalText : MonoBehaviour
+public class DisplayGoal : MonoBehaviour
 {
-    // HappinessKeeper happinessKeeper;
     Gnome gnomeHappiness;
     public TextMeshProUGUI displayGoal;
     public TMP_InputField inputGoal;
     public GameObject inputField;
+    public GameObject toggleBox;
+    public GameObject markCompleteText;
+    public GameObject goalEnterButton;
+
+    [SerializeField] Toggle toggle;
+
 
     void Start()
     {
+        toggle = GetComponent<Toggle>();
+        markGoalComplete();
         gnomeHappiness = FindObjectOfType<Gnome>();
+        toggleBox.SetActive(false);
+        markCompleteText.SetActive(false);
     }
 
     public void setGoal()
@@ -21,5 +31,20 @@ public class DisplayGoalText : MonoBehaviour
         gnomeHappiness.AddToHappiness();
         displayGoal.text = "Your goal: " + inputGoal.text;
         inputField.SetActive(false);
+        goalEnterButton.SetActive(false);
+        toggleBox.SetActive(true);
+        markCompleteText.SetActive(true);
+    }
+
+    public void markGoalComplete()
+    {
+        if (toggle.isOn)
+        {
+            Debug.Log("toggle is on");
+        }
+        else if (!toggle.isOn)
+        {
+            Debug.Log("toggle is off");
+        }
     }
 }
